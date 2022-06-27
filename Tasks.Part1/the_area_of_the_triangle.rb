84d69b6a6879de6a0af174e78ba37b72705aeb6a
broomank <107892214+broomank@users.noreply.github.com>
2022-06-27 20:06:05 +0300

    Add files via upload

---
+++ Tasks.Part1/the_area_of_the_triangle.rb
@@ -0,0 +1,9 @@
+puts 'Enter a base of the triangle(cm)'
+base = gets.chomp
+
+puts 'Enter a height of the triangle(cm)'
+height = gets.chomp
+
+area = (base.to_i*height.to_i)/2
+
+puts "The area of the triangle is #{area} square cm."
