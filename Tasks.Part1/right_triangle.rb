7bf207e5b76be5fba7c47ebbdb10ad90dd3f8dd3
broomank <107892214+broomank@users.noreply.github.com>
2022-06-29 11:42:38 +0300

    Update right_triangle.rb

--- Tasks.Part1/right_triangle.rb
+++ Tasks.Part1/right_triangle.rb
@@ -1,20 +1,15 @@
 puts 'Enter side A'
 side_a = Integer(gets.chomp)
-
 puts 'Enter side B'
 side_b = Integer(gets.chomp)
-
 puts 'Enter side C'
 side_c = Integer(gets.chomp)
-
 if side_a ** 2 == side_b ** 2 + side_c ** 2 || side_b ** 2 == side_a ** 2 + side_c ** 2 || side_c ** 2 == side_a ** 2 + side_b ** 2
-	puts'Треугольник прямоугольный'
+  puts'Треугольник прямоугольный'
 end
-
 if side_a == side_b or side_b == side_c or side_a == side_c
-	puts'Треугольник равнобедренный'
+  puts'Треугольник равнобедренный'
 end
-
 if side_a == side_b && side_b == side_c
-	puts 'Треугольник равносторонний'
+  puts 'Треугольник равносторонний'
 end
