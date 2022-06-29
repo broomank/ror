8c9f8f4301dd89c2e29844dc80bc00638917f7b4
broomank <107892214+broomank@users.noreply.github.com>
2022-06-29 12:22:19 +0300

    Update right_triangle.rb

--- Tasks.Part1/right_triangle.rb
+++ Tasks.Part1/right_triangle.rb
@@ -1,15 +1,15 @@
 puts 'Enter side A'
-side_a = Integer(gets.chomp)
+side_a = gets.chomp
 puts 'Enter side B'
-side_b = Integer(gets.chomp)
+side_b = gets.chomp
 puts 'Enter side C'
-side_c = Integer(gets.chomp)
-if side_a ** 2 == side_b ** 2 + side_c ** 2 || side_b ** 2 == side_a ** 2 + side_c ** 2 || side_c ** 2 == side_a ** 2 + side_b ** 2
-  puts'Треугольник прямоугольный'
+side_c = gets.chomp
+if side_a.to_i**2 == side_b.to_i**2 + side_c.to_i**2 || side_b.to_i**2 == side_a.to_i**2 + side_c.to_i**2 || side_c.to_i**2 == side_a.to_i**2 + side_b.to_i**2
+	puts'Треугольник прямоугольный'
 end
-if side_a == side_b or side_b == side_c or side_a == side_c
-  puts'Треугольник равнобедренный'
+if side_a.to_i == side_b.to_i or side_b.to_i == side_c.to_i or side_a.to_i == side_c.to_i
+	puts'Треугольник равнобедренный'
 end
-if side_a == side_b && side_b == side_c
-  puts 'Треугольник равносторонний'
+if side_a.to_i == side_b.to_i && side_b.to_i == side_c.to_i
+	puts 'Треугольник равносторонний'
 end
