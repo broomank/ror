438eaa87a291e0ea651f03962934bba127427aad
broomank <107892214+broomank@users.noreply.github.com>
2022-06-29 11:46:02 +0300

    Update the_area_of_the_triangle.rb

--- Tasks.Part1/the_area_of_the_triangle.rb
+++ Tasks.Part1/the_area_of_the_triangle.rb
@@ -1,6 +1,6 @@
 puts 'Enter a base of the triangle(cm)'
-base = Integer(gets.chomp)
+base = gets.chomp
 puts 'Enter a height of the triangle(cm)'
-height = Integer(gets.chomp)
-area = (base*height)/2
+height = gets.chomp
+area = (base.to_i*height.to_i)/2
 puts "The area of the triangle is #{area} square cm."
