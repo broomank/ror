de684d70c791d919217ea3900f806583279777b1
broomank <107892214+broomank@users.noreply.github.com>
2022-06-29 12:17:43 +0300

    Update quadratic_equation.rb

--- Tasks.Part1/quadratic_equation.rb
+++ Tasks.Part1/quadratic_equation.rb
@@ -4,12 +4,12 @@ puts 'введите коэфициент B'
 b = Integer(gets.chomp)
 puts 'введите коэфициент C'
 c = Integer(gets.chomp)
-d = b ** 2 - ( 4 * a * c)
+d = b**2 - (4 * a * c)
 if d < 0
   puts "Дискриминант=#{d}, корней нет"
 elsif d > 0
-  х1 = ( - b + Math.sqrt(d)) / (2 * a)
-  x2 = ( - b - Math.sqrt(d)) / (2 * a)
+  х1 = (- b + Math.sqrt(d))/(2 * a)
+  x2 = (- b - Math.sqrt(d))/(2 * a)
   puts "Дискриминант=#{d}, X1=#{х1} and X2=#{x2}" 
 else
   x1 = - b / ( 2 * a )
