43f25bc78fd3ce9ddf82fcebc8ad9ee0f85bfcd9
broomank <107892214+broomank@users.noreply.github.com>
2022-06-29 12:13:10 +0300

    Update optimal_weight.rb

--- Tasks.Part1/optimal_weight.rb
+++ Tasks.Part1/optimal_weight.rb
@@ -2,7 +2,7 @@ puts "What's your name?"
 name = gets.chomp
 puts "What's your height(cm)?"
 height = gets.chomp
-optimal_weight = (height.to_i- 110) * 1.15
+optimal_weight = (height.to_i - 110) * 1.15
   if optimal_weight < 0
     puts "Your weight is optimal"
   else
